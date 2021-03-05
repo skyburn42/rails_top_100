be05bb51ca1065a0d4ecf7880dbaecc56b11a44d
Tyler Cheney <tyler.cheney.05@gmail.com>
2021-03-05 00:28:33 -0700

    Got models and databases working

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,4 @@
 class Artist < ApplicationRecord
-  belongs_to :Billboard
+  belongs_to :billboard
   has_many :songs, dependent: :destroy
 end
--- db/migrate/20210305072320_create_songs.rb
+++ db/migrate/20210305072320_create_songs.rb
@@ -2,7 +2,7 @@ class CreateSongs < ActiveRecord::Migration[6.1]
   def change
     create_table :songs do |t|
       t.string :title
-      t.belongs_to :Artist, null: false, foreign_key: true
+      t.belongs_to :artist, null: false, foreign_key: true
 
       t.timestamps
     end
