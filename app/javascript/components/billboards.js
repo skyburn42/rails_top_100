55d671bda1d3d7b32d745bee67628be7b3ba83b7
Sky B <skyburn5616@gmail.com>
2021-03-10 15:24:14 -0800

    additional styles for nav and backgroun, added styles for billboard and artist

--- app/javascript/components/billboards.js
+++ app/javascript/components/billboards.js
@@ -1,28 +1,35 @@
 import React from 'react';
+
+
+
 const url = "http://localhost:3000/billboards/";
 const Billboards = ({ billboards }) => {
   return (
-    <>
+    <div className="mycards">
       
       <h1>Your Favorites</h1>
       <a href={"/billboards/new"}>Create Your Billboard</a>
-      
-      { billboards.map( (billboard) => 
-        <div>
-          <h3>{billboard.title}</h3>
-          <a href={url + billboard.id}>Show</a>
-          <br />
-          <a href={url + billboard.id + "/edit"}>Edit</a>
-          <br />
-          <a 
-            href={url + billboard.id }
-            data-method="delete"
-          >
-            Delete
-          </a>
+      <div className="cardList">
+        { billboards.map( (billboard) => 
+        <div className="holy">
+            
+            <h3 className="holyh">{billboard.title}</h3>
+            <img className="imgTest" src="../../assets/images/tst.png"></img>
+               
+            <div className="holycard">
+              <a className="links"href={url + billboard.id}>Show</a>
+              <br />
+              <a className="links" href={url + billboard.id + "/edit"}>Edit</a>
+              <br />
+                <a className="links"
+                  href={url + billboard.id }
+                  data-method="delete">Delete
+                </a>
+          </div>
         </div>
-      )}
-    </>
+        )}
+      </div>
+    </div>
   )
 }
 export default Billboards;
